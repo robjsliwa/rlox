a614a8232815ab1f8d87680faac6c07a1647c92b
robjsliwa <robjsliwa@gmail.com>
2020-08-30 20:10:44 -0400

    Macro to generate expression structs.

--- src/rlox/mod.rs
+++ src/rlox/mod.rs
@@ -1,9 +1,13 @@
 mod error_handler;
+
+#[macro_use]
+mod generate_ast;
 mod literal;
 mod token;
 mod token_type;
 
 pub use self::error_handler::*;
+pub use self::generate_ast::*;
 pub use self::literal::Literal;
 pub use self::token::*;
 pub use self::token_type::*;
